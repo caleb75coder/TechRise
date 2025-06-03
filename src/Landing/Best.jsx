fc2bc424f42429437721baa1621e8f874b9705b2
caleb75coder <mmuochukwukamiracle@gmail.com>
2025-06-03 19:15:16 +0100

    jcs

--- src/Landing/Best.jsx
+++ src/Landing/Best.jsx
@@ -5,13 +5,10 @@ const Best = () => {
     <main>
       <div className="flex flex-col justify-center items-center ">
         <h1 className="text-4xl text-black font-bold">Best Trip And Prices</h1>
-        <p className="text-lg">
+        <p>
           At <strong className="text-amber-400">Drive Konacuv</strong> transport
-          and logistics, we are dedicated to providing a
-        </p>
-        <p className="text-lg ">
-          Smart fast relieable solution in transportation and logistics across
-          Nigeria
+          and logistics, we are dedicated to providing a Smart fast relieable
+          solution in transportation and logistics across Nigeria
         </p>
       </div>
     </main>
